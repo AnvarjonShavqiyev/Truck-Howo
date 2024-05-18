3b99e5e66c8f09f68073af052d539d6359bebd14
GITHUBDAGI <129954447+watshihaharuki@users.noreply.github.com>
2024-05-18 21:46:59 +0500

    styles added

--- src/pages/SingleCategory/SingleCategory.jsx
+++ src/pages/SingleCategory/SingleCategory.jsx
@@ -4,22 +4,41 @@ import data from '../../db/products';
 import './SingleCategory.scss'
 import Navbar from '../../components/navbar/Navbar'
 import Footer from '../../components/footer/Footer'
+import Container from '../../utils/Utils';
 const SingleCategory = () => {
   const { name } = useParams();
   const [category, setCategory] = useState([]);
-
+  const [currentCategory, setCurrentCategory] = useState(name)
   useEffect(() => {
-    const newData = data.products.filter(product => name === product.title);
+    const newData = data.products.filter(product => currentCategory === product.title);
     setCategory(newData[0]);
-  }, [name]);
+  }, [name, currentCategory]);
+  const handleChange = (event) => {
+    console.log(1)
+    setCurrentCategory(event.target.value);
+  };
   console.log(category)
   return (
     category.data && 
     <div>
       <Navbar/>
-      {/* <Products/> */}
+      <div className='single-cat-header__wrapper'>
+        <Container>
+          <div className='single-cat__header'>
+          <h4 className='single-category-title'>{category.title}</h4>
+          <select className='single-cat-selection' value={currentCategory} onChange={handleChange}>
+            <option value="Аксессуар">Аксессуар</option>
+            <option value="Тормозные части">Тормозные части</option>
+            <option value="Фильтра">Фильтра</option>
+            <option value="Кузовные детали">Кузовные детали</option>
+            <option value="Электрические запчасти">Электрические запчасти</option>
+            <option value="Запчасти для ДВС">Запчасти для ДВС</option>
+          </select>
+          </div>
+        </Container>
+      </div>
+      <Container>
       <div className='single-cat__wrapper'>
-        <h4 className='single-category-title'>{category.title}</h4>
         <div className='all__products__wrapper'>
           {category.data.map((product, index) => (
             <div className='product__wrapper' key={index}>
@@ -29,6 +48,7 @@ const SingleCategory = () => {
           ))}
         </div>
       </div>
+      </Container>
       <Footer/>
     </div>
   );
