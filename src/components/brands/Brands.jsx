a3f96b05962e118107460775f1186495d43b087f
GITHUBDAGI <129954447+watshihaharuki@users.noreply.github.com>
2024-05-20 11:27:53 +0500

    map added

--- src/components/brands/Brands.jsx
+++ src/components/brands/Brands.jsx
@@ -7,9 +7,19 @@ import wabco from '../../assets/photo/logo-wabco.jpeg'
 import qinyan from '../../assets/photo/qinyan-logo.jpg'
 import sachs from '../../assets/photo/SACHS_original.webp'
 import { useTranslation } from 'react-i18next'
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 const Brands = () => {
   const { t } = useTranslation()
+  const mapStyles = {
+    height: "500px",
+    width: "100%"
+  };
+
+  const defaultCenter = {
+    lat: 41.23228776566744,
+    lng: 69.21575043819631
+  };
   return (
     <Container>
       <div className='brands__wrapper' id='brands'>
@@ -31,6 +41,15 @@ const Brands = () => {
             <img src={sachs}></img>
           </div>
         </div>
+      </div>
+      <div className='google-map__wrapper'>
+        <LoadScript googleMapsApiKey="AIzaSyD7R0Z4PmibH-7aVLDWrfNRiXiyvOss40Q">
+          <GoogleMap
+            mapContainerStyle={mapStyles}
+            zoom={18}
+            center={defaultCenter}
+          />
+        </LoadScript>
       </div>
     </Container>
   )
